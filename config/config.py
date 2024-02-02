73e673a919c51d3894c5333d5c9983dcc5ad00b6
Gorbunov <dinodigital@yandex.ru>
2024-02-02 23:36:50 +0300

    v1.0.2 - added TELEGRAM_BOT_SESSION_NAME

--- config/config.py
+++ config/config.py
@@ -10,7 +10,7 @@ USER_DB_NAME = "user.db"
 ROOT_DIR = Path(__file__).resolve().parent.parent
 USER_DB_PATH = os.path.join(ROOT_DIR, f"data/{USER_DB_NAME}")
 
-
+TELEGRAM_BOT_SESSION_NAME = "MyBot"
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TG_API_ID = int(os.environ.get('TG_API_ID'))
 TG_API_HASH = os.environ.get('TG_API_HASH')
--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from loguru import logger
 from pyrogram import Client
 
-from config.config import TELEGRAM_BOT_TOKEN, TG_API_ID, TG_API_HASH
+from config import config
 from jobs.scheduler import scheduler
 from data.models import create_db_if_not_exists
 
 plugins = dict(root="handlers")
-app = Client("MyBot",
-             api_id=TG_API_ID,
-             api_hash=TG_API_HASH,
-             bot_token=TELEGRAM_BOT_TOKEN,
+app = Client(config.TELEGRAM_BOT_SESSION_NAME,
+             api_id=config.TG_API_ID,
+             api_hash=config.TG_API_HASH,
+             bot_token=config.TELEGRAM_BOT_TOKEN,
              plugins=plugins)
 
 # Запуск приложения
